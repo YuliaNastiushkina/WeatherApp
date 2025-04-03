e3b079a268f9ddf6ef5df7482e687fca602a79f3
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-04-02 21:25:50 -0400

    created `URLSessionExtention`

--- WeatherApp/NetworkService/Extention/URLSessionExtention.swift
+++ WeatherApp/NetworkService/Extention/URLSessionExtention.swift
@@ -1,8 +1,12 @@
-//
-//  URLSessionExtention.swift
-//  WeatherApp
-//
-//  Created by Yulya on 2025-04-02.
-//
-
 import Foundation
+
+extension URLSession: RequestProtocol {
+    /// Fetches data from the given URL asynchronously.
+    /// This method conforms `URLSession` to `RequestProtocol`, allowing it to be used as a network service for making API requests.
+    /// - Parameter url: The URL from which to fetch data.
+    /// - Returns: A tuple containing the raw `Data` and the `URLResponse` received from the server.
+    /// - Throws: An error if the network request fails.
+    func fetchData(from url: URL) async throws -> (Data, URLResponse) {
+        try await data(from: url)
+    }
+}
