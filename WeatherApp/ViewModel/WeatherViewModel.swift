5cad772a25b39bae85a0b73ce27a30692611e9c2
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-04-02 21:25:22 -0400

    updated ViewModel and added doc comments

--- WeatherApp/ViewModel/WeatherViewModel.swift
+++ WeatherApp/ViewModel/WeatherViewModel.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// Responsible for fetching and managing weather data.
 @MainActor
 class WeatherViewModel: ObservableObject {
     @Published var cityName: String = ""
@@ -10,8 +11,15 @@ class WeatherViewModel: ObservableObject {
     @Published var errorMessage: String?
     @Published var feelsLike: String = ""
     
-    private let weatherService = Request()
-    
+    /// Fetches weather data for the specified city.
+    ///
+    /// - If `cityName` is empty, sets `errorMessage` to `"Please enter a city name"` and exits.
+    /// - Updates `isLoading` to `true` while fetching data.
+    /// - On success:
+    ///     - Updates `temperature`, `feelsLike`, `description`, and `humidity` with formatted data.
+    /// - On failure:
+    ///     - Sets `errorMessage` to `"Failed to fetch weather data"`.
+    /// - Finally, sets `isLoading` to `false` when the request is complete.
     func fetchWeather() async {
         guard !cityName.isEmpty else {
             errorMessage = "Please enter a city name"
@@ -32,5 +40,12 @@ class WeatherViewModel: ObservableObject {
         }
         
         isLoading = false
+    }
+    
+    //MARK: Private interface
+    private let weatherService: Request
+    
+    init(networkService: RequestProtocol) {
+        self.weatherService = Request(networkService: networkService)
     }
 }
