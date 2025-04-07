135c6de4dd5a19b33a04b7ed58b524c77e61efe3
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-04-07 15:29:52 -0400

    created SavedWeatherModel

---
+++ WeatherApp/Model/SavedWeatherModel.swift
@@ -0,0 +1,25 @@
+import Foundation
+import SwiftData
+
+/// A model representing saved weather data for a specific city.
+@Model
+class SavedWeatherModel {
+    /// The name of the city.
+    var city: String
+    /// The current temperature in Celsius.
+    var temperature: String
+    /// A detailed description of the current weather condition (e.g., "clear sky").
+    var weatherDescription: String
+    /// The feels-like temperature in Celsius.
+    var feelsLike: String
+    /// Humidity in percentage.
+    var humidity: String
+    
+    init(city: String, temperature: String, description: String, feelsLike: String, humidity: String) {
+        self.city = city
+        self.temperature = temperature
+        self.weatherDescription = description
+        self.feelsLike = feelsLike
+        self.humidity = humidity
+    }
+}
